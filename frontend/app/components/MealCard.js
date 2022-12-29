657242949361c2537d5fb2616130b5a6a544e937
dianap321 <dianap@post.bgu.ac.il>
2022-12-29 18:46:57 +0200

    meal card

--- frontend/app/components/MealCard.js
+++ frontend/app/components/MealCard.js
@@ -5,6 +5,10 @@ const MealCard = ({item}) => {
     return (
         <View style={styles.container}>
             <View style={styles.card}>
+                <Image
+                    source={{uri:item.image}}
+                    style={styles.cardImage}
+                />
                 <View style={styles.cardContent}>
                     <View style={styles.cardTextContent}>
                         <Text style={styles.cardTitle}>{item.name}</Text>
@@ -12,10 +16,7 @@ const MealCard = ({item}) => {
                     </View>
                     {/* Add your other card content here */}
                 </View>
-                <Image
-                    source={require('frontend/app/assets/meatPizza.png')}
-                    style={styles.cardImage}
-                />
+
             </View>
         </View>
     );
@@ -27,25 +28,30 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginVertical: 10,
+        // elevation: 2,
+
         // marginHorizontal: 10,
 
     },
     card: {
         // width: 300,
-        height: 150,
+        height: 120,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-evenly',
-        borderRadius: 8,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.3,
-        shadowRadius: 8,
+        borderRadius:10,
+
+        backgroundColor: "white",
+        // shadowColor: '#000',
+        // shadowOffset: { width: 0, height: 2 },
+        // shadowOpacity: 0.3,
+        // shadowRadius: 8,
         elevation: 5,
     },
     cardContent: {
         flex: 1,
         padding: 16,
+        // fontFamily: 'Rubik-Regular',
     },
     cardTextContent: {
         flex: 1,
@@ -61,9 +67,10 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     cardImage: {
-        width: 120,
-        height: 120,
+        width: 102,
+        height: '85%',
         borderRadius: 8,
+        marginLeft: 12
         // resizeMode: "contain"
     },
 });
