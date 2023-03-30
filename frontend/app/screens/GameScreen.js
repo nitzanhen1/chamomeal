c4792d2d2d5d174c4a6addc955c08034a76fa49d
nitzanhen1 <nitzanhe@post.bgu.ac.il>
2023-03-30 08:58:13 +0300

    commit flowers images

--- frontend/app/screens/GameScreen.js
+++ frontend/app/screens/GameScreen.js
@@ -5,11 +5,11 @@ import {useSelector} from "react-redux";
 export default function GameScreen() {
     const {badges, first_name, score} = useSelector(state => state.mealReducer);
     const badge_details = [
-        {id:1, badge: badges[0], source: require('../assets/badges/flower6.png'), text: '10 פרחים', showText: false},
-        {id:2, badge: badges[1], source: require('../assets/badges/flower6.png'), text: '20 פרחים', showText: false},
-        {id:3, badge: badges[2], source: require('../assets/badges/flower6.png'), text: '50 פרחים', showText: false},
-        {id:4, badge: badges[3], source: require('../assets/badges/flower6.png'), text: '100 פרחים', showText: false},
-        {id:5, badge: badges[4], source: require('../assets/badges/flower6.png'), text: '200 פרחים', showText: false},
+        {id:1, badge: badges[0], source: require('../assets/badges/flower1.png'), text: '10 פרחים', showText: false},
+        {id:2, badge: badges[1], source: require('../assets/badges/flower2.png'), text: '20 פרחים', showText: false},
+        {id:3, badge: badges[2], source: require('../assets/badges/flower3.png'), text: '50 פרחים', showText: false},
+        {id:4, badge: badges[3], source: require('../assets/badges/flower4.png'), text: '100 פרחים', showText: false},
+        {id:5, badge: badges[4], source: require('../assets/badges/flower5.png'), text: '200 פרחים', showText: false},
         {id:6, badge: badges[5], source: require('../assets/badges/flower6.png'), text: '350 פרחים', showText: false},
         {id:7, badge: badges[6], source: require('../assets/badges/flower6.png'), text: '500 פרחים', showText: false},
         {id:8, badge: badges[7], source: require('../assets/badges/flower6.png'), text: '750 פרחים', showText: false},
