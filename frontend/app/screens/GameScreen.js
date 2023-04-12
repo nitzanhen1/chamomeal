0a71589f0af214dc48c916e942840bc5cf98271a
nitzanhen1 <nitzanhe@post.bgu.ac.il>
2023-04-12 17:25:51 +0300

    update badges

--- frontend/app/screens/GameScreen.js
+++ frontend/app/screens/GameScreen.js
@@ -5,26 +5,26 @@ import {useSelector} from "react-redux";
 export default function GameScreen() {
     const {badges, first_name, score} = useSelector(state => state.mealReducer);
     const badge_details = [
-        {id:1, badge: badges[0], source: require('../assets/badges/flower1.png'), text: '10 פרחים', showText: false},
-        {id:2, badge: badges[1], source: require('../assets/badges/flower2.png'), text: '50 פרחים', showText: false},
-        {id:3, badge: badges[2], source: require('../assets/badges/flower3.png'), text: '100 פרחים', showText: false},
-        {id:4, badge: badges[3], source: require('../assets/badges/flower4.png'), text: '200 פרחים', showText: false},
-        {id:5, badge: badges[4], source: require('../assets/badges/flower5.png'), text: '500 פרחים', showText: false},
-        {id:6, badge: badges[5], source: require('../assets/badges/flower6.png'), text: '1000 פרחים', showText: false},
+        {id:1, badge: badges[0], source: require('../assets/badges/flower6.png'), text: '5 קיימות', showText: false},
+        {id:2, badge: badges[1], source: require('../assets/badges/flower6.png'), text: '15 קיימות', showText: false},
+        {id:3, badge: badges[2], source: require('../assets/badges/flower6.png'), text: '30 קיימות', showText: false},
+        {id:4, badge: badges[3], source: require('../assets/badges/flower6.png'), text: '50 קיימות', showText: false},
+        {id:5, badge: badges[4], source: require('../assets/badges/flower6.png'), text: '80 קיימות', showText: false},
+        {id:6, badge: badges[5], source: require('../assets/badges/flower6.png'), text: '120 קיימות', showText: false},
 
-        {id:7, badge: badges[6], source: require('../assets/badges/flower6.png'), text: '5 קיימות', showText: false},
-        {id:8, badge: badges[7], source: require('../assets/badges/flower6.png'), text: '15 קיימות', showText: false},
-        {id:9, badge: badges[8], source: require('../assets/badges/flower6.png'), text: '30 קיימות', showText: false},
-        {id:10, badge: badges[9], source: require('../assets/badges/flower6.png'), text: '50 קיימות', showText: false}, //TODO delete or add badges
-        {id:11, badge: badges[10], source: require('../assets/badges/flower6.png'), text: '80 קיימות', showText: false}, //TODO delete or add badges
-        {id:12, badge: badges[11], source: require('../assets/badges/flower6.png'), text: '120 קיימות', showText: false}, //TODO delete or add badges
+        {id:7, badge: badges[6], source: require('../assets/badges/flower1.png'), text: '10 פרחים', showText: false},
+        {id:8, badge: badges[7], source: require('../assets/badges/flower2.png'), text: '50 פרחים', showText: false},
+        {id:9, badge: badges[8], source: require('../assets/badges/flower3.png'), text: '100 פרחים', showText: false},
+        {id:10, badge: badges[9], source: require('../assets/badges/flower4.png'), text: '200 פרחים', showText: false},
+        {id:11, badge: badges[10], source: require('../assets/badges/flower5.png'), text: '500 פרחים', showText: false},
+        {id:12, badge: badges[11], source: require('../assets/badges/flower6.png'), text: '1000 פרחים', showText: false},
 
-        {id:13, badge: badges[12], source: require('../assets/badges/flower6.png'), text: '2 התחברות', showText: false}, //TODO delete or add badges
-        {id:14, badge: badges[13], source: require('../assets/badges/flower6.png'), text: '4 התחברות', showText: false}, //TODO delete or add badges
-        {id:15, badge: badges[14], source: require('../assets/badges/flower6.png'), text: '7 התחברות', showText: false}, //TODO delete or add badges
-        {id:16, badge: badges[15], source: require('../assets/badges/flower6.png'), text: '10 התחברות', showText: false}, //TODO delete or add badges
-        {id:17, badge: badges[16], source: require('../assets/badges/flower6.png'), text: '14 התחברות', showText: false}, //TODO delete or add badges
-        {id:18, badge: badges[17], source: require('../assets/badges/flower6.png'), text: '20 התחברות', showText: false}, //TODO delete or add badges
+        {id:13, badge: badges[12], source: require('../assets/badges/flower6.png'), text: '2 התחברות', showText: false},
+        {id:14, badge: badges[13], source: require('../assets/badges/flower6.png'), text: '4 התחברות', showText: false},
+        {id:15, badge: badges[14], source: require('../assets/badges/flower6.png'), text: '7 התחברות', showText: false},
+        {id:16, badge: badges[15], source: require('../assets/badges/flower6.png'), text: '10 התחברות', showText: false},
+        {id:17, badge: badges[16], source: require('../assets/badges/flower6.png'), text: '14 התחברות', showText: false},
+        {id:18, badge: badges[17], source: require('../assets/badges/flower6.png'), text: '20 התחברות', showText: false},
     ]
     const [visibleTextMap, setVisibleTextMap] = useState({});
     const renderItem = ({item}) => {
